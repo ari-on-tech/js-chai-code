1a8a0b5ac183077a680132b7c324ccf463df56c7
Arijit Sural <arijitsural2004@gmail.com>
2025-12-16 21:58:11 +0530

    Create conversion.js

---
+++ 1-basic/conversion.js
@@ -0,0 +1,32 @@
+let score = true;
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+
+//"33"=>33
+// "33ab"=>NaN. It will not convert properly
+// ture=>1;false=>0
+
+let isLoggedIN=""
+console.log(typeof isLoggedIN);
+
+
+let booleanIsLoggedIn=Boolean(isLoggedIN)
+console.log(typeof booleanIsLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+// boolean conversion
+// 1 =>true; 0 =>false
+// "Arijit"=>ture
+
+let someNumber=404;
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
